a5c7c562d1ed56534f2f302266e493d3f5dfa4d9
VitinhoVls <victorsedlmaier14@gmail.com>
2024-04-29 19:56:51 -0300

    commit1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import {db}from './firebase.js';
+import React, {useEffect, useState} from 'react';
+import instagram from '../src/instagram-logo.png';
+import Header from './Header';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+
+useEffect(()=>{
+  
+},[])
+
+
+
+  return (
+    <div className="App">
+  
+      <Header user={user}></Header>
+    </div>
+  );
+}
+
+export default App;
